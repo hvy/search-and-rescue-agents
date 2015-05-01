c42cc4f3d165a51bc12d86aca48f1b46524b3645
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-05-01 17:31:17 +0200

    created empty scripts and objects

---
+++ search-and-rescue-agents/Assets/Scripts/GridEnvironment.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * One instance maintained by the BaseStation
+ */
+public class GridEnvironment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
