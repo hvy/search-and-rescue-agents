155b488573697cd07568f75bfde03545f71f442c
Tobias Wikström <tob.wikstrom@gmail.com>
2015-05-01 18:45:30 +0200

    Agent additions

--- search-and-rescue-agents/Assets/Scripts/Agent.cs
+++ search-and-rescue-agents/Assets/Scripts/Agent.cs
@@ -6,20 +6,38 @@ public class Agent : MonoBehaviour {
     private bool carryingTarget; // Agent is carrying a target
     private Human currentTarget; // Target to rescue
 
+    private List<GNode> path; //
+
 	// Use this for initialization
 	void Start () {
     	carryingTarget = false;
+    	path = new ArrayList<GNode>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		// FIXME
+		scan();
 		sendEnvironmentData();
+
+		if (carryingTarget)
+		    moveToEntrance();
+		else if (currentTarget != null)
+		    moveToTarget();
+		else
+		    search();
+
+		collisionAvoidance();
 
 	}
 
 	void OnCollisionEnter2D(Collision2D coll){
        // FIXME
+    }
+
+	// Scan with CircleCollision2D, collect objects.
+    private void scan() {
+    	// FIXME
     }
 
 	private void pickUpTarget() {
@@ -31,15 +49,24 @@ public class Agent : MonoBehaviour {
     }
 
 	private void sendEnvironmentData() {
-		// FIXME
+		// FIXME send data to Base
 	}
 
-	private void moveToExit() {
-
+	private void moveToEntrance() {
+       // FIXME
 	}
 
 	private void moveToTarget() {
     	// FIXME
+	}
+
+	private void collisionAvoidance() {
+       // FIXME collision avoidance AI
+	}
+
+	// Assign a target to this agent. Should be decided by the Base.
+	public void assignTarget(Human target) {
+		currentTarget = human;
 	}
 
 }
