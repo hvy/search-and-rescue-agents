350b4ea7c472a7559a2f519f22ad5752bebe0df7
Tobias Wikström <tob.wikstrom@gmail.com>
2015-05-02 19:01:31 +0200

    Added obstacles and using circles for agents for now

--- search-and-rescue-agents/Assets/Scripts/Environment.cs
+++ search-and-rescue-agents/Assets/Scripts/Environment.cs
@@ -4,10 +4,19 @@ using System.Collections.Generic;
 
 public class Environment {
 
+	public int width, height;
 	public List<Human> humans;
+	public List<Vector2> entrances;
+	public List<Vector2> obstacles;
+
 	public List<GameObject> gameObjects;
 
-	public Environment () {
+	public Environment (int height, int width, List<Human> humans, List<Vector2> entrances, List<Vector2> obstacles) {
+		this.height = height;
+		this.width = width;
+		this.humans = humans;
+		this.entrances = entrances;
+		this.obstacles = obstacles;
 		
 	}
 	
--- search-and-rescue-agents/Assets/Scripts/EnvironmentFactory.cs
+++ search-and-rescue-agents/Assets/Scripts/EnvironmentFactory.cs
@@ -6,13 +6,46 @@ public class EnvironmentFactory {
 
 	public static Environment createBasicEnvironment () {
 
+		// Create ground
+		int width = 50;
+		int height = 50;
 		GameObject ground = GameObject.CreatePrimitive (PrimitiveType.Cube);
 		ground.transform.position = new Vector3 (0, 0, 1.0f);
-		ground.transform.localScale = new Vector3 (50.0f, 50.0f, 1);
+		ground.transform.localScale = new Vector3 (height, width, 1);
 		ground.transform.parent = GameObject.Find ("_Environment").transform;
 		ground.name = "ground";
 
-		Environment env = new Environment ();
+		// Create obstacles
+		List<Vector2> obstacles = new List<Vector2>();
+
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(-2,3)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(-1,3)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(0,3)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(1,3)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(2,3)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(3,3)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(4,3)));
+
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(4,2)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(4,1)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(4,0)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(4,-1)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(4,-2)));
+
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(3,-2)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(2,-2)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(1,-2)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(0,-2)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(-1,-2)));
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(-2,-2)));
+
+		obstacles.Add(ObstacleFactory.spawnObstacleAt(new Vector2(-2,2)));
+
+		List<Vector2> entrances = new List<Vector2>();
+
+		entrances.Add(new Vector2(-2,1));
+
+		Environment env = new Environment (height, width, new List<Human>(), entrances, obstacles);
 
 		return env;
 	}
