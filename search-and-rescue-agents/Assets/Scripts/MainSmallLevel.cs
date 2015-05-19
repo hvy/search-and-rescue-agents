1076ee63bce348f7de9d44469d8772673d1c9395
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-05-19 22:49:40 +0200

    Made sure all three scenes are working

--- search-and-rescue-agents/Assets/Scripts/MainSmallLevel.cs
+++ search-and-rescue-agents/Assets/Scripts/MainSmallLevel.cs
@@ -7,31 +7,29 @@ using System.Collections.Generic;
  * It then waits for the user input to create the rescue agents (mouse clicks)
  * and then to start the simulation (space bar).
  */
-public class Main : MonoBehaviour {
-
+public class MainSmallLevel : MonoBehaviour {
+	
 	public Vector2 environmentPosition;
-
+	
 	private int height, width;
 	private BaseStation baseStation;
 	
 	void Start () {
-	
+		
 		Debug.Log ("[INFO] Starting main...");
-
+		
 		// Find out the dimensions of the environment
 		GameObject envBase = GameObject.Find ("EnvironmentBase");
 		width = (int) envBase.transform.localScale.x;
 		height = (int) envBase.transform.localScale.y;
-
+		
 		Debug.Log ("[INFO] Found environment");
 		Debug.Log ("[INFO] Width: " + width);
 		Debug.Log ("[INFO] Height: " + height);
 
 		List<Vector2> entrances = new List<Vector2> ();
-		entrances.Add (new Vector2(48.0f, 0.3f));
-		entrances.Add (new Vector2(49.0f, 0.3f));
-		entrances.Add (new Vector2(50.0f, 0.3f));
-		entrances.Add (new Vector2(51.0f, 0.3f));
+		entrances.Add (new Vector2(8.0f, 0.3f));
+		entrances.Add (new Vector2(9.0f, 0.3f));
 
 		// Find the base station game object and add the entrance information and the grid representation of the environment
 		baseStation = (BaseStation) GameObject.Find ("BaseStation").GetComponent(typeof(BaseStation));
@@ -40,18 +38,18 @@ public class Main : MonoBehaviour {
 		GridEnvironment gridEnv = new GridEnvironment(height, width);
 		baseStation.setGridEnvironment (gridEnv);
 	}
-
+	
 	void Update () {
 		// On mouse click, create a rescue agent
 		if (Input.GetButtonDown ("Fire1")) {
 			Vector3 clickPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+			
 			clickPos.z = 0;
-
+			
 			// Create the agent and add a reference to the base station
 			Agent agent = AgentFactory.spawnAgentAt (clickPos);
 			agent.setBase(baseStation);
-
+			
 			// Register the agent in the base station
 			baseStation.addAgent(agent);
 		}
