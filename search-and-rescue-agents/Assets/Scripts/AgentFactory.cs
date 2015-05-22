88aeff6cd806706e1cc49db5692f25ebf065ede5
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2015-05-22 08:57:33 +0200

    Added a medium level

--- search-and-rescue-agents/Assets/Scripts/AgentFactory.cs
+++ search-and-rescue-agents/Assets/Scripts/AgentFactory.cs
@@ -4,11 +4,9 @@ using System.Collections;
 public class AgentFactory : MonoBehaviour {
 
 	public static Agent spawnAgentAt (Vector3 pos) {
-
-//		Transform prefab = Resources.Load("Robot Kyle/Model/Robot Kyle", typeof(Transform)) as Transform;
+		
 		Transform prefab = Resources.Load("Prefabs/Agent", typeof(Transform)) as Transform;
 		Transform agent = GameObject.Instantiate (prefab, pos, Quaternion.LookRotation (Vector3.up)) as Transform;
-//		Debug.Log (agent);
 		agent.SetParent (GameObject.Find ("_Agents").transform);
 		agent.gameObject.name = "agent";
 		agent.gameObject.GetComponent<Renderer>().material.color = Color.blue;
